7646eacd50a8049040a4a06681b4a94749629b75
sokolovstanislav <ignateva-1946@mail.ru>
2024-03-13 17:06:10 +0300

    VERILOGStart_01
    Простой проект на VERILOG

---
+++ VERILOGStart/VERILOGStart.v
@@ -0,0 +1,17 @@
+module VERILOGStart(led,clk);
+	output led;
+	input clk;
+	
+	reg led;
+	reg[24:0] counter;
+
+	always@(posedge clk)	
+	begin
+		counter<=counter+1;
+		if(counter==25'd25000000)
+		begin
+				led<=~led;
+				counter<=0;
+		end
+	end
+endmodule
